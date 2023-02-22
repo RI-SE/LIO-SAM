d7e766705694838cb7ba64bad426f9fbac68436f
Oscar Johansson <oscar.johansson@astazero.com>
2023-02-22 10:59:32 +0100

    Added save map cmd

---
+++ cmd/saveMap.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+cd ..
+
+echo "Enter save folder name "
+read folderName
+
+
+echo "Saving maps to $HOME/"LIO-SAM_Maps/$folderName/" and converting to georeferenced las file" 
+
+ros2 service call /lio_sam/save_map "resolution: 0.0
+destination: '/LIO-SAM_Maps/$folderName/'"
+
+python FillJson.py $HOME/"LIO-SAM_Maps/$folderName/GlobalMap.pcd"
+
+echo "Converting to Las file.."
+
+pdal pipeline $HOME/"LIO-SAM_Maps/$folderName/ConvertLasToPcdWithGeoRef.json"
+
+echo "Done."
+
+echo "Las info: "
+
+pdal info $HOME/"LIO-SAM_Maps/$folderName/GlobalMap.las"
+
